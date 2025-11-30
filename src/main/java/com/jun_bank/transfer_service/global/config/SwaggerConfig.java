124ced5fb9268af1baf3bb19086e82ff75c7d1d5
jun <pickjog@gmail.com>
2025-11-30 18:10:35 +0900

    feat(global-config): SwaggerConfig 추가

---
+++ src/main/java/com/jun_bank/transfer_service/global/config/SwaggerConfig.java
@@ -0,0 +1,45 @@
+package com.jun_bank.transfer_service.global.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.servers.Server;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
+
+/**
+ * Swagger/OpenAPI 문서화 설정
+ */
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI openAPI() {
+        String securitySchemeName = "bearerAuth";
+
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Transfer Service API")
+                        .description("Jun-Bank 이체 서비스 API")
+                        .version("v1.0.0")
+                        .contact(new Contact()
+                                .name("Jun-Bank Team")
+                                .email("support@jun-bank.com")))
+                .servers(List.of(
+                        new Server().url("/").description("Current Server")))
+                .addSecurityItem(new SecurityRequirement().addList(securitySchemeName))
+                .components(new Components()
+                        .addSecuritySchemes(securitySchemeName,
+                                new SecurityScheme()
+                                        .name(securitySchemeName)
+                                        .type(SecurityScheme.Type.HTTP)
+                                        .scheme("bearer")
+                                        .bearerFormat("JWT")
+                                        .description("JWT 토큰을 입력하세요")));
+    }
+}
